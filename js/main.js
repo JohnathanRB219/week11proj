21ea373b97497e014270bac12508b0e5991e43fe
JohnathanRB219 <johnathanrbaldwin@gmail.com>
2018-07-18 06:34:26 -0700

    editing2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $("#menu").click(function(){
+	// open and closes my button
 	$("#menu").toggleClass("menu-open");
+// collapses my menu button to open and close
 	$("#menu2").toggleClass("menu-collapsed");
-	$("#menu2 ul li").toggleClass("hidden animated fadeInLeft");
+	// lis fade in from the left when button is open 
+	$("#menu2 ul").toggle("hidden animated fadeInLeft");
 });
