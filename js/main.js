c40a0ce4613534f1c915ed5e5c3f3fbc9b2355f4
JohnathanRB219 <johnathanrbaldwin@gmail.com>
2018-07-22 13:55:37 -0700

    editing

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $("#menu").click(function(){
 	$("#menu").toggleClass("menu-open");
 // collapses my menu button to open and close
 	$("#menu2").toggleClass("menu2open");
+	$("#menu2").toggleClass("transitionOut")
 	// lis fade in from the left when button is open 
-	$("#menu2 ul li").toggleClass("hidden animated fadeInLeft");
+	$("#menu2 ul li").toggleClass("hidden animated fadeInUp");
 });
